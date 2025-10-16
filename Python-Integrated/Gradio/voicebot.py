e49eb67bbb88f5acb7c8fe7ad67cbf5b04590d6c
Abhishek Pathak <abhishekpathak37733@gmail.com>
2025-10-16 10:30:27 +0530

    updated folder

--- Python-Integrated/Gradio/voicebot.py
+++ Python-Integrated/Gradio/voicebot.py
@@ -165,10 +165,10 @@ with gr.Blocks() as demo:
         outputs=[stt_output, doc_response, doc_voice, chat_output]
     )
 # ASGI app expose for Vercel
-app = gr.mount_gradio_app(app=None, blocks=demo, path="/")
+# app = gr.mount_gradio_app(app=None, blocks=demo, path="/")
 
 # Local dev ke liye
-if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+# if __name__ == "__main__":
+#     demo.launch(server_name="0.0.0.0", server_port=7860)
 
-# demo.launch(debug=True)
+demo.launch(debug=True)
