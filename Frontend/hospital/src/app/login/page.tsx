a28f1c711c48698356947f8d4592ced608292ab9
Abhishek Pathak <abhishekpathak37733@gmail.com>
2025-10-24 21:27:43 +0530

    updated login code

--- Frontend/hospital/src/app/login/page.tsx
+++ Frontend/hospital/src/app/login/page.tsx
@@ -66,7 +66,8 @@ export default function Login() {
         {
           ...formData,
           role,
-        }
+        },
+        { withCredentials: true }
       );
 
       if (response.status === 200) {
