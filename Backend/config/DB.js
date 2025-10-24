3b6ccbe9fa2188e9af83fa6550195fb4a06ead69
Abhishek Pathak <abhishekpathak37733@gmail.com>
2025-10-24 17:02:25 +0530

    updated vercel file

--- Backend/config/DB.js
+++ Backend/config/DB.js
@@ -1,39 +1,12 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MongoString);
-//     console.log("MongoDB Connected Successfully");
-//   } catch (error) {
-//     console.error("MongoDB Connection Failed:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
 import mongoose from "mongoose";
 
-let cachedDb = null; // cache the connection
-
 const connectDB = async () => {
-  if (cachedDb) {
-    // Use existing connection
-    return cachedDb;
-  }
-
   try {
-    const conn = await mongoose.connect(process.env.MongoString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    cachedDb = conn;
+    await mongoose.connect(process.env.MongoString);
     console.log("MongoDB Connected Successfully");
-    return conn;
   } catch (error) {
     console.error("MongoDB Connection Failed:", error.message);
-    throw new Error(error.message); // don't exit process in serverless
+    process.exit(1);
   }
 };
 
