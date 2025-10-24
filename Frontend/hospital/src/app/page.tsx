be5b7921bcbab5cd454628f05cfdb2c7d55e2267
Abhishek Pathak <abhishekpathak37733@gmail.com>
2025-10-24 18:42:24 +0530

    updated code to allow type any

--- Frontend/hospital/src/app/page.tsx
+++ Frontend/hospital/src/app/page.tsx
@@ -1,7 +1,7 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import Services from "@/components/Services";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
 
 export default function Home() {
   return (
