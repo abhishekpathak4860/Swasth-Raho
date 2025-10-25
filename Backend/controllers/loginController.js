2864ec6945e21ed3ab8aa27f744317d0e7998c8c
Abhishek Pathak <abhishekpathak37733@gmail.com>
2025-10-25 20:02:19 +0530

    updated login code

--- Backend/controllers/loginController.js
+++ Backend/controllers/loginController.js
@@ -42,7 +42,6 @@ export const loginUser = async (req, res) => {
       httpOnly: true, // cannot be accessed by JS
       secure: process.env.NODE_ENV === "production" ? true : false, // true for HTTPS
       sameSite: "none",
-      domain: ".vercel.app",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
