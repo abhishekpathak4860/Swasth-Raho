235d2b5c85103e47aa396c906a9adf59b27144f8
Abhishek Pathak <abhishekpathak37733@gmail.com>
2025-10-25 23:58:45 +0530

    updated code

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/DB.js";
 import registerRoute from "./routes/registerRoute.js";
