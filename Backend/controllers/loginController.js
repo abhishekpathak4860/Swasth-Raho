9782c90fbeaf7d9fe9ea1a9d74edf1aef030a9df
Abhishek Pathak <abhishekpathak37733@gmail.com>
2025-10-25 22:50:28 +0530

    updated code

--- Backend/controllers/loginController.js
+++ Backend/controllers/loginController.js
@@ -40,7 +40,7 @@ export const loginUser = async (req, res) => {
     // Store token in cookie
     res.cookie("token", token, {
       httpOnly: true, // cannot be accessed by JS
-      secure: process.env.NODE_ENV === "production" ? true : false, // true for HTTPS
+      secure: true, // true for HTTPS
       sameSite: "none",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
